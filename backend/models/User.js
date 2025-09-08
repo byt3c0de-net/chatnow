9f5cea30cebeb078fcfc90356a1f67fa15dc9960
byt3c0de <228401750+byt3c0de-net@users.noreply.github.com>
2025-09-08 20:06:45 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,11 @@
+// backend/models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-  isVerified: { type: Boolean, default: false },
-  verificationCode: { type: String },
-  createdAt: { type: Date, default: Date.now }
+  password: { type: String, required: true },
+  verified: { type: Boolean, default: false },
+  verificationCode: { type: String }
 });
 
 module.exports = mongoose.model('User', userSchema);
